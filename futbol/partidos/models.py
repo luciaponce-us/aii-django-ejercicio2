2d65aded9b056fba735a783d8f8bcf44f030d73b
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2025-11-17 14:02:57 +0100

    Base de datos poblada

--- futbol/partidos/models.py
+++ futbol/partidos/models.py
@@ -5,7 +5,7 @@ class Partido(models.Model):
     visitante = models.CharField(max_length=100)
     goles_local = models.IntegerField(default=0)
     goles_visitante = models.IntegerField(default=0)
-    jornada = models.IntegerField()
+    jornada = models.ForeignKey('Jornada', on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.equipo_local} vs {self.equipo_visitante} en Jornada {self.jornada}"
   
